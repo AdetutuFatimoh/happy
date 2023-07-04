79c343f856b9521afd589373386de700c4a914ba
AdetutuFatimoh <adetutufatimoh@gmail.com>
2023-07-04 12:28:49 -0700

    feat:commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
+import Image from 'next/image'
+import pic from './images/group.jpg'
 
 export default function page() {
   return (
-    <div>my page</div>
+    <div>
+      <h2>student</h2>
+      <Image src={pic} alt='a group' width={600} height={400}/>
+    </div>
   )
 }
